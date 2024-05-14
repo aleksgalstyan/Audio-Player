80dc6fba282502bf0545eca6444bb9722b3d2c26
aleksgalstyan <169162159+aleksgalstyan@users.noreply.github.com>
2024-05-14 17:17:50 +0400

    Update script.js

--- audioplayer_Aleks.Galstyan/script.js
+++ audioplayer_Aleks.Galstyan/script.js
@@ -1,46 +1,114 @@
 let data = {
     title: [
-        "Xatuba Karavan",
-        "Xatuba Hay qajer",
+        "Xatuba-Karavan",
+        "Xatuba-Hay qajer",
         "Kate Bush-Army Dreamers"
     ],
     song: [
         "music/XATUBA-KARAVAN-world75.spcs.bio.mp3",
         "music/Xatuba-Hay_qajer-world75.spcs.bio.mp3",
-        "music/Kate_Bush_-_Army_Dreamers_@BaseNaija.mp3"],
+        "music/Kate_Bush_-_Army_Dreamers_@BaseNaija.mp3"
+    ],
     poster: [
-        "https://media.soundoflife.com/articles/476/An_Introduction_To_Minimalism_Article_cover.gif",
-        "https://cdn.dribbble.com/users/508806/screenshots/7847569/media/ab03a3f581f175ac3c4ae322ba08d754.gif",
+        "https://images.hdqwalls.com/wallpapers/desert-moon-4k-lk.jpg",
+        "https://images.hdqwalls.com/wallpapers/desert-moon-4k-lk.jpg",
         "https://courtneyssoundworld.files.wordpress.com/2015/05/kate-bush.gif",
-    ]
+    ],
 }
 
 let currentSong = 0
-let song= new Audio()
-window.onload = function(){
-    playSong()
-}
-function playSong (){
-    song.src = data.song[currentSong];
-    let songTitle = document.getElementById("songtitle");   
-    songTitle.textContent = data.title[currentSong];
-    let img = document.getElementsByClassName("row1");
-    img[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")";
-    let main = document.getElementsByClassName("main");
-    main[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")";
+let song = new Audio()
+
+function playSong() {
+    song.src = data.song[currentSong]
+    let songTitle = document.getElementById("songtitle")
+    songTitle.textContent = data.title[currentSong]
+    let img = document.getElementsByClassName("row1")
+    img[0].style.backgroudImage = "url(" + data.poster[currentSong] + ")";
+    let main = document.getElementsByClassName("main")
+    main[0].style.backgroudImage = "url(" + data.poster[currentSong] + ")";
     song.play()
 }
+function next(){
+    currentSong += 1
+    if(currentSong >= data.song.length){
 
-function playOrPauseSong(){
+        currentSong = 0
+    }
+    song.src = data.song[currentSong]
+    let songTitle = document.getElementById("songtitle")
+    songTitle.textContent = data.title[currentSong]
+    let img = document.getElementsByClassName("row1")
+    img[0].style.backgroudImage = "url(" + data.poster[currentSong] + ")";
+    let main = document.getElementsByClassName("main")
+    main[0].style.backgroudImage = "url(" + data.poster[currentSong] + ")";
+
+}
+function pre(){
+    currentSong -= 1 
+     if(currentSong < 0){
+
+        currentSong = data.song.length - 1
+    }
+    song.src = data.song[currentSong]
+    let songTitle = document.getElementById("songtitle")
+    songTitle.textContent = data.title[currentSong]
+    let img = document.getElementsByClassName("row1")
+    img[0].style.backgroudImage = "url(" + data.poster[currentSong] + ")";
+    let main = document.getElementsByClassName("main")
+    main[0].style.backgroudImage = "url(" + data.poster[currentSong] + ")";
+  
+
+}
+
+function playOrPauseSong() {
     let play = document.getElementById("play")
-    if(song.paused){
+    if (song.paused) {
         song.play()
         play.src = "images/pause.png"
     }
-    else{
+    else {
         song.pause()
         play.src = "images/play-button-arrowhead.png"
-    }    
+    }
 
+
+}
+window.onload = function () {
+    playSong()
+}
+
+song.addEventListener("timeupdate", function () {
+    let fill = document.getElementsByClassName("fill")
+    let position = song.currentTime / song.duration
+    fill[0].style.marginLeft = position * 100 + "%"
+    converTime(song.currentTime)
+    if(song.ended){
+        next()
+    }
+})
+
+
+
+function converTime(seconds) {
+    currentTime = document.getElementsByClassName("currentTime")
+    let min = Math.floor(seconds/60)
+    let sec = Math.floor(seconds%60)
+    min = (min<10) ? "0" + min : min;
+    sec = (sec<10) ? "0" + sec : sec;
     
+    currentTime[0].textContent = min + ':' + sec 
+
+    totalTime(song.duration)
+}
+
+function totalTime(seconds){
+    
+    let min = Math.floor(seconds/60)
+    let sec = Math.floor(seconds%60)
+    min = (min<10) ? "0" + min : min;
+    sec = (sec<10) ? "0" + sec : sec;
+
+    currentTime[0].textContent += "/" + min + ':' + sec
+
 }
